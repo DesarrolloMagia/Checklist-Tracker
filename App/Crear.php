e9fb32c3dd5c9b23f980c42539a2377072cd1e97
Isaac Samaniego <isaac.samaniego@utp.ac.pa>
2023-10-24 17:50:52 -0500

    3: update and add end time

--- App/Crear.php
+++ App/Crear.php
@@ -5,6 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST['tarea-nombre'];
     $descripcion = $_POST['tarea-descripcion'];
     $fecha_compromiso = $_POST['fecha-de-compromiso'];
+    $hora_culminacion = $_POST['hora-culminacion']; // Nuevo campo para la hora de culminación
     $prioridad = $_POST['tarea-prioridad'];
     $responsable = $_POST['tarea-responsable'];
 
@@ -12,8 +13,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($nombre)) {
         header("Location: ../Dashboard.php?mess=error");
     } else {
-        $stmt = $conn->prepare("INSERT INTO tareas (nombre, descripcion, fecha_compromiso, prioridad, responsable) VALUES (?, ?, ?, ?, ?)");
-        $res = $stmt->execute([$nombre, $descripcion, $fecha_compromiso, $prioridad, $responsable]);
+        $stmt = $conn->prepare("INSERT INTO tareas (nombre, descripcion, fecha_compromiso, hora_culminacion, prioridad, responsable) VALUES (?, ?, ?, ?, ?, ?)");
+        $res = $stmt->execute([$nombre, $descripcion, $fecha_compromiso, $hora_culminacion, $prioridad, $responsable]);
 
         if ($res) {
             header("Location: ../Dashboard.php?mess=success");
