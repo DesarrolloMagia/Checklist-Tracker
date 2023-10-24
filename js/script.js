db3dfbbb2834103d59d58acec7c69dac24d4bbe3
Isaac Samaniego <isaac.samaniego@utp.ac.pa>
2023-10-24 17:52:20 -0500

    3: update reload after drop

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ function updateTaskStatus(taskId, newStatus) {
         },
         success: function(response) {
             console.log("Tarea actualizada con éxito.");
+            location.reload();
         },
         error: function(error) {
             console.error("Error al actualizar la tarea.");
@@ -22,9 +23,14 @@ $(document).ready(function() {
             var taskId = ui.item.attr("id").split("-")[1]; // Obtiene el ID de la tarea
             var targetKanbanBlock = ui.item.parent().attr("id");
 
-            if (targetKanbanBlock === "procesos") {
+            // Obtiene el estado actual de la tarea
+            var currentState = ui.item.data("estado");
+
+            if (targetKanbanBlock === "pendientes") {
+                updateTaskStatus(taskId, 'Pendiente');
+            } else if (targetKanbanBlock === "procesos" && currentState !== 'Pendiente') {
                 updateTaskStatus(taskId, 'En Proceso');
-            } else if (targetKanbanBlock === "completados") {
+            } else if (targetKanbanBlock === "completados" && currentState === 'En Proceso') {
                 updateTaskStatus(taskId, 'Completado');
             }
         }
